191922d4546ecb7bb9202371db0d0edc22330ace
Przemysław <Przemysaw@ps>
2015-06-18 15:32:35 +0200

    - resources

--- awpto-battleship/src/main/java/pl/edu/us/ii/awpto/App.java
+++ awpto-battleship/src/main/java/pl/edu/us/ii/awpto/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Board board = new Board("maps.txt");
+        Gameplay game = new Gameplay(board);
+        
+        board.showPlayerOneBoard();
+        board.showPlayerTwoBoard();
+        System.out.print(game.isHit(0, 9, 0));
+        board.showPlayerOneBoard();
+        game.setHit(0, 9, 0, game.isHit(0, 9, 0));
+        board.showPlayerOneBoard();
+        System.out.println(game.checkGame());
     }
 }
